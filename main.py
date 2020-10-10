6416195cf8a54d21b0525d969c1a6448e5f5974c
Bill Tanthowi Jauhari <bill.tanthowi@majoo.id>
2020-10-10 21:27:27 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, url_for, session, redirect, render_template
+from markupsafe import escape
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'ML'
+mysql = MySQL(app)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/create", methods=['GET'])
+def create():
+    return render_template('create.html')
+
+@app.route("/store", methods=['POST'])
+def store():
+    details = request.form
+    administrative = details['administrative']
+    administrative_duration = details['administrative_duration']
+    informational = details['informational']
+    informational_duration = details['informational_duration']
+    productrelated = details['productrelated']
+    productrelated_duration = details['productrelated_duration']
+    bouncesrates = details['bouncesrates']
+    exitrates = details['exitrates']
+    pagevalues = details['pagevalues']
+    specialday = details['specialday']
+    month = details['month']
+    operatingsystems = details['operatingsystems']
+    browser = details['browser']
+    region = details['region']
+    traffictype = details['traffictype']
+    visitortype = details['visitortype']
+    weekend = details['weekend']
+    revenue = details['revenue']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO profil(administrative, administrative_duration, informational, informational_duration, productrelated, productrelated_duration, bouncesrates, exitrates, pagevalues, specialday, month, operatingsystems, browser, traffictype, visitortype, weekend, revenue) VALUES (%s, %s)", (administrative, administrative_duration, informational, informational_duration, productrelated, productrelated_duration, bouncesrates, exitrates, pagevalues, specialday, month, operatingsystems, browser, region, traffictype, weekend, revenue))
+    mysql.connection.commit()
+    cur.close()
+    return 'you store the data'
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == "bill":
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        else:
+            return 'wrong username'
+    else:
+        return '''
+            <form action="/login" method="POST">
+                <input type="text" name="username">
+                <button>login</button>
+            </form>
+        '''
+
